7cf5d3d847b6d8b7eae834da7bfbf6f1766bacc8
a4mbs <116489535+a4mbs@users.noreply.github.com>
2023-12-16 00:16:05 +0200

    step 001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# A4mbsArt
+# A4mbs
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.7.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,14 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import { initFlowbite } from 'flowbite';
-import { UpdateTitleService } from './services/update-title.service';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'a4mbs-root',
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styles: []
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  
-
-  showScrollButton = false;
-
-
-  constructor(private titleService: UpdateTitleService) {}
-
-  ngOnInit(): void {
-    initFlowbite();
-    this.titleService.init();
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if (window.scrollY || document.documentElement.scrollTop || document.body.scrollTop > 200) {
-      this.showScrollButton = true;
-    } else if (this.showScrollButton && window.scrollY || document.documentElement.scrollTop || document.body.scrollTop < 10) {
-      this.showScrollButton = false;
-    }
-  }
-
-  scrollToTop() {
-    (window as any).scroll({ top: 0, left: 0, behavior: 'smooth' });
-  }
-
-
+export class AppComponent {
+  title = 'a4mbs';
 }
